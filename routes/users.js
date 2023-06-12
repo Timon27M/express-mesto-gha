c6c03b5345121cd50c3ca3b508d721cff126e72f
Timofey Pichugin <tpich87@gmail.com>
2023-06-12 10:30:33 +0300

    feat: добавлена авторизация исправленая валидация

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,16 @@ const {
   getCurrentUser,
 } = require('../controllers/users');
 
+const {
+  validatorUserId,
+  validatorUpdateProfile,
+  validatorUpdateAvatar,
+} = require('../middlewares/validator');
+
 router.get('/users', getUsers);
-router.get('/users/:userId', getUser);
+router.get('/users/:userId', validatorUserId, getUser);
 router.get('/users/me', getCurrentUser);
-router.patch('/users/me', updateProfile);
-router.patch('/users/me/avatar', updateAvatar);
+router.patch('/users/me', validatorUpdateProfile, updateProfile);
+router.patch('/users/me/avatar', validatorUpdateAvatar, updateAvatar);
 
 module.exports = router;
